214aa168bf0551dea3616d2ef1a039763f39cfd1
b9019940 <b9019940@newcastle.ac.uk>
2021-11-16 16:11:01 +0000

    fixed colour usage to be print and colour blind friendly

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ def house_price_comparison():
     title = "Average house sale prices in London compared to Newcastle upon Tyne 2001 - 2021"
     fig, (ax1, ax2) = plt.subplots(1, 2)
     fig.suptitle(title, fontsize=12)
-    ax1.plot(pd.to_datetime(london_detached["Date"]), london_detached["averagePrice"], label="Detached")
-    ax1.plot(pd.to_datetime(london_semi_detached["Date"]), london_semi_detached["averagePrice"], label="Semi detached")
-    ax1.plot(pd.to_datetime(london_terraced["Date"]), london_terraced["averagePrice"], label="Terraced")
-    ax1.plot(pd.to_datetime(london_flats["Date"]), london_flats["averagePrice"], label="Flat")
+    ax1.plot(pd.to_datetime(london_detached["Date"]), london_detached["averagePrice"], label="Detached", color="#a6cee3")
+    ax1.plot(pd.to_datetime(london_semi_detached["Date"]), london_semi_detached["averagePrice"], label="Semi detached", color="#1f78b4")
+    ax1.plot(pd.to_datetime(london_terraced["Date"]), london_terraced["averagePrice"], label="Terraced", color="#b2df8a")
+    ax1.plot(pd.to_datetime(london_flats["Date"]), london_flats["averagePrice"], label="Flat", color="#33a02c")
     ax1.legend()
     ax1.set_title("London", fontsize=12)
     ax1.grid(True)
     ax1.set_xlabel("Year of sale")
     ax1.set_ylabel("Sale price")
-    ax2.plot(pd.to_datetime(newcastle_detached["Date"]), newcastle_detached["averagePrice"], label="Detached")
+    ax2.plot(pd.to_datetime(newcastle_detached["Date"]), newcastle_detached["averagePrice"], label="Detached", color="#a6cee3")
     ax2.plot(pd.to_datetime(newcastle_semi_detached["Date"]), newcastle_semi_detached["averagePrice"],
-             label="Semi detached")
-    ax2.plot(pd.to_datetime(newcastle_terraced["Date"]), newcastle_terraced["averagePrice"], label="Terraced")
-    ax2.plot(pd.to_datetime(newcastle_flats["Date"]), newcastle_flats["averagePrice"], label="Flat")
+             label="Semi detached", color="#1f78b4")
+    ax2.plot(pd.to_datetime(newcastle_terraced["Date"]), newcastle_terraced["averagePrice"], label="Terraced", color="#b2df8a")
+    ax2.plot(pd.to_datetime(newcastle_flats["Date"]), newcastle_flats["averagePrice"], label="Flat", color="#33a02c")
     ax2.legend()
     ax2.set_title("Newcastle", fontsize=12)
     ax2.grid(True)
@@ -45,12 +45,14 @@ def broadband_comparison():
     file = "C:\\Users\\surfy\\PycharmProjects\\CSC3833\\B_broadbandData_2021\\B_broadbandData_2021\\202006_fixed_laua_performance_wrangled.csv"
     data = pd.read_csv(file)
     m, b = np.polyfit(data["averageDown"], data["averageUpload"], 1)
-    plt.plot(data["averageDown"], m*data["averageDown"] + b, color="red", label="Line of best fit")
-    plt.scatter(data["averageDown"], data["averageUpload"], s=1)
+    correlation = "The correlation between a regions average upload and download speeds is " + str(data["averageDown"].corr(data["averageUpload"]))
+    plt.plot(data["averageDown"], m*data["averageDown"] + b, color="#1b9e77", label="Line of best fit")
+    plt.scatter(data["averageDown"], data["averageUpload"], s=1, color="#d95f02")
     plt.xlabel("Average download speed (Mbit/s)")
     plt.ylabel("Average upload speed (Mbit/s)")
     plt.legend()
-    plt.title("A comparison of broadband upload and download speeds")
+    plt.text(0, 0, correlation)
+    plt.title("A comparison of regional average broadband upload and download speeds")
     plt.show()
 
 
@@ -59,4 +61,4 @@ def stock_exchange():
 
 
 if __name__ == '__main__':
-    broadband_comparison()
+   broadband_comparison()
